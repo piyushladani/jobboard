2f06149807035ef620db0c47de8ca421da1dd522
piyushladani <ladanipiyush00@gmail.com>
2017-09-04 13:19:36 +0200

    debugKit remove

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -212,6 +212,4 @@ Type::build('timestamp')
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
-if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
-}
+
